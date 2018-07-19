cbfc7c13150cefa4ec6bfb2908e6ce7d2a7b2292
om <om@om-PC>
2018-07-19 16:18:35 +0530

    hi

---
+++ maven1/src/test/java/maven1/maven1/rk.java
@@ -0,0 +1,17 @@
+package maven1.maven1;
+
+import org.testng.annotations.Test;
+
+public class rk {
+	@Test
+	public void test()
+	{
+		System.out.println("Test");
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("Test1");
+	}
+}
+
