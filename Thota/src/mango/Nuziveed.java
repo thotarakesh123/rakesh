7ea7d5fc465872ad05add3e2c279b9a9975b1051
om <om@om-PC>
2018-07-19 15:48:39 +0530

    adding

---
+++ Thota/src/mango/Nuziveed.java
@@ -0,0 +1,22 @@
+package mango;
+
+import org.testng.annotations.Test;
+
+public class Nuziveed {
+
+	
+		@Test
+		public void test()
+		{
+			System.out.println("Test");
+		}
+		@Test
+		public void test1()
+		{
+			System.out.println("Test1");
+		}
+}
+	
+	
+
+
